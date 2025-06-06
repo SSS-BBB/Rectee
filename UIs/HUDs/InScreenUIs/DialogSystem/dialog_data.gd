220c0a47a895275d6dfae5c44251d2e0957fc3cd
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2025-06-06 16:30:38 +0700

    Finished Box 1
    -Finished adding Box 1
    -Added box2-box5

--- UIs/HUDs/InScreenUIs/DialogSystem/dialog_data.gd
+++ UIs/HUDs/InScreenUIs/DialogSystem/dialog_data.gd
@@ -2,12 +2,10 @@ class_name DialogData extends Resource
 
 # Class variables
 @export var speaker: Texture
-@export var text: String
+@export var translate_text: bool = true
+@export var text: String:
+	get():
+		if translate_text:
+			return tr(text)
+		return text
 @export var text_typing_duration: float = 0.5
-
-# Statics
-#static func new_dialog_data(new_speaker: Texture, new_text: String) -> DialogData:
-	#var dialog_data: DialogData
-	#dialog_data.speaker = new_speaker
-	#dialog_data.text = new_text
-	#return dialog_data
